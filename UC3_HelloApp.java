d6aca1ee2f28a335c4ea9861000da180db7f0753
Getha Varshini <varshinigetha@gmail.com>
2026-03-26 14:06:01 +0530

    Coded UC3: Display Hello with Command-Line Argument or Default Message

---
+++ UC3_HelloApp.java
@@ -0,0 +1,6 @@
+public class UC3_HelloApp {
+    public static void main(String[] args) {
+        String name = (args.length > 0) ? args[0] : "World";
+        System.out.println("Hello, " + name + "!");
+    }
+}
